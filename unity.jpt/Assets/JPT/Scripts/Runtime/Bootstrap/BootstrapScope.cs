e85292960f5aa233c81554f06961d5af21332fad
johnn <johnnyzaev@gmail.com>
2025-03-26 20:03:41 +0400

    Add logger, VContainer and Odin

---
+++ unity.jpt/Assets/JPT/Scripts/Runtime/Bootstrap/BootstrapScope.cs
@@ -0,0 +1,13 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace JPT.Scripts.Runtime.Bootstrap
+{
+    public class BootstrapScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            base.Configure(builder);
+        }
+    }
+}
---
+++ unity.jpt/Assets/JPT/Scripts/Runtime/Utilities/Logger/Log.cs
@@ -0,0 +1,13 @@
+﻿namespace JPT.Scripts.Runtime.Utilities.Logger
+{
+    public static class Log
+    {
+        public static readonly TagLog Default = new(string.Empty);
+        public static readonly TagLog Loading = new("Loading");
+        public static readonly TagLog Boot = new("Bootstrap");
+        public static readonly TagLog Meta = new("Menu");
+        public static readonly TagLog Battle = new("Game");
+
+        public static readonly BuilderLogPool Builder = new(new TagLog(string.Empty), 5);
+    }
+}
