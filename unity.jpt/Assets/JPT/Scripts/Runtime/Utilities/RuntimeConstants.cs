74099ef526e02034ac193e8ac596eb3821e8585d
johnn <johnnyzaev@gmail.com>
2025-03-26 21:26:22 +0400

    rename scenes and add SceneManager.cs and RuntimeConstants.cs

---
+++ unity.jpt/Assets/JPT/Scripts/Runtime/Utilities/RuntimeConstants.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace JPT.Scripts.Runtime.Utilities
+{
+    public static class RuntimeConstants
+    {
+        public static class Scenes
+        {
+            public static readonly int Bootstrap = SceneUtility.GetBuildIndexByScenePath("0.Bootstrap");
+            public static readonly int Loading = SceneUtility.GetBuildIndexByScenePath("1.Loading");
+            public static readonly int Meta = SceneUtility.GetBuildIndexByScenePath("2.Menu");
+            public static readonly int Core = SceneUtility.GetBuildIndexByScenePath("3.Game");
+            public static readonly int Empty = SceneUtility.GetBuildIndexByScenePath("4.Empty");
+        }
+    }
+}
